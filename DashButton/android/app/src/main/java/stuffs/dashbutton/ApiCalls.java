b754d6d15655b959c38dcf914af790b24f27c3d6
Stefan Huth <stefan-huth@Stefans-MacBook-Pro.local>
2017-08-28 11:53:30 +0100

    android: hitting duff url working

--- DashButton/android/app/src/main/java/stuffs/dashbutton/ApiCalls.java
+++ DashButton/android/app/src/main/java/stuffs/dashbutton/ApiCalls.java
@@ -1,10 +1,11 @@
 package stuffs.dashbutton;
 
+import retrofit.Callback;
 import retrofit.http.GET;
 
 public interface ApiCalls {
 
     @GET("/test")
-    void test();
+    void test(Callback<String> callback);
 
 }
--- DashButton/android/app/src/main/java/stuffs/dashbutton/DashActivity.java
+++ DashButton/android/app/src/main/java/stuffs/dashbutton/DashActivity.java
@@ -1,13 +1,16 @@
 package stuffs.dashbutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import com.squareup.okhttp.OkHttpClient;
+import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.OkClient;
+import retrofit.client.Response;
 
 public class DashActivity extends AppCompatActivity {
 
@@ -22,17 +25,28 @@ public class DashActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 System.out.println("AMG I've Been Hit");
-
+                hitApi();
             }
         });
     }
 
     private void hitApi() {
-        RestAdapter adapter = new RestAdapter.Builder().setEndpoint("localhost").setClient(new OkClient(
+        RestAdapter adapter = new RestAdapter.Builder().setEndpoint("http://localhost3000").setClient(new OkClient(
                 new OkHttpClient())).setLogLevel(RestAdapter.LogLevel.FULL).build();
 
         ApiCalls apiCalls =  adapter.create(ApiCalls.class);
-        
+
+        apiCalls.test(new Callback<String>() {
+            @Override
+            public void success(String s, Response response) {
+                Toast.makeText(DashActivity.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(DashActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
